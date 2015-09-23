68766111d8de66eda7e6913958da3f334d3d5e6d
zHeney <buseles@ya.ru>
2015-09-23 17:58:05 +0300

    up

---
+++ player.php
@@ -0,0 +1,31 @@
+<!-- PLAYER 1 -->
+				<div id="jquery_jplayer_1" class="jp-jplayer" ></div>
+				<div id="jp_container_1" class="jp-audio">
+					<?php
+					require 'structurePlayer.php';
+					?>
+				</div>
+
+<!-- PLAYER 2 -->
+				<div id="jquery_jplayer_2" class="jp-jplayer" ></div>
+				<div id="jp_container_2" class="jp-audio">
+					<?php
+					require 'structurePlayer.php';
+					?>
+				</div>
+
+<!-- PLAYER 3 -->
+				<div id="jquery_jplayer_3" class="jp-jplayer" ></div>
+				<div id="jp_container_3" class="jp-audio">
+					<?php
+					require 'structurePlayer.php';
+					?>
+				</div>
+
+<!-- PLAYER 4 -->
+				<div id="jquery_jplayer_4" class="jp-jplayer" ></div>
+				<div id="jp_container_4" class="jp-audio">
+					<?php
+					require 'structurePlayer.php';
+					?>
+				</div>
